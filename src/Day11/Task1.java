9040a1b1fe5c9199dda02bb8ffe500433ad5eeed
Omer Sari <saromer96@gmail.com>
2022-07-30 08:08:59 -0500

    Updated

--- src/Day11/Task1.java
+++ src/Day11/Task1.java
@@ -4,29 +4,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class _02_SwitchingNestedFrame {
-
+public class Task1 {
     public static void main(String[] args) {
-
         System.setProperty("webdriver.chrome.driver", "/Users/omersari/Desktop/selenium-java-4/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://chercher.tech/practice/frames");
 
-        driver.switchTo().frame("frame1");
+        driver.switchTo().frame("frame2");
 
-        driver.switchTo().frame("frame3");
+        WebElement IDropDown = driver.findElement(By.cssSelector("select[id='animals']"));
 
-        WebElement checkbox = driver.findElement(By.id("a"));
-        checkbox.click();
+        Select select = new Select(IDropDown);
 
-        driver.switchTo().defaultContent(); // switch to main webpage
-
-        WebElement mainElement = driver.findElement(By.xpath("//a[text()='UiPath']"));
-        mainElement.click();
+        select.selectByVisibleText("Avatar");
 
     }
-
 }
